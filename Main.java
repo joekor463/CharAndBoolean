119c666fe59231df585ad6a8dd7b9997cc77f390
Evgeny Korobenkov <joekor463@gmail.com>
2020-05-08 11:47:18 +0800

    char and boolean

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char Mychar = '\u00AE';
+	    System.out.println("Копирайт символ - " + Mychar);
+    }
+}
